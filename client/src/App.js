bccf1eade74ae970a2021bd5cbaabcd4c34fb495
Subhocr07 <96890352+Subhocr07@users.noreply.github.com>
2022-09-28 13:53:16 +0530

    Completed all backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import {BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css';
 import Login from './components/Login'
 import Register from './components/Register'
-import Profile from './components/Profile'
 import Error from './components/Error'
 
 function App() {
@@ -10,9 +9,8 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
-        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/" element={<Login/>} />
         <Route exact path="/register" element={<Register/>} />
-        <Route exact path="/" element={<Profile/>} />
         <Route exact path="*" element={<Error/>} />
       </Routes>
       </BrowserRouter>
